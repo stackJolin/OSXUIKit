d6c49aac2d6161ac5b41eb603c3b66b2f0071a5a
zhuhoulin <zhuhoulin@windimg.com>
2019-12-25 22:06:25 +0800

    Initial commit

---
+++ Example/OSXUIKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OSXUIKit
+//
+//  Created by stackjolin on 12/25/2019.
+//  Copyright (c) 2019 stackjolin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/OSXUIKit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OSXUIKit
+//
+//  Created by stackjolin on 12/25/2019.
+//  Copyright (c) 2019 stackjolin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
